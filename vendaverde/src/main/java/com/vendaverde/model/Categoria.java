8ba14555f97b2533f60fd5b545968c97a74d1d34
mastruzz <72388859+mastruzz@users.noreply.github.com>
2020-11-09 08:30:48 -0300

    Adição das API's  de construção do DB.
    Atualização com as API's de população do banco de dados.

--- vendaverde/src/main/java/com/vendaverde/model/Categoria.java
+++ vendaverde/src/main/java/com/vendaverde/model/Categoria.java
@@ -1,13 +1,21 @@
 package com.vendaverde.model;
+import java.util.List;
 
+
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
+@Table(name = "tb_categoria")
 public class Categoria {
 	
 	
@@ -15,24 +23,44 @@ public class Categoria {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
-	@NotNull
+	
 	@Size(min = 1, max = 150)
 	private String nome;
 	
-	@NotNull
+	
 	private boolean ativo;
+
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
 	
-	@NotNull
-	@Size(min = 1, max = 200)
-	private String produtor;
+	private List<Produto> produto;	
 	
-	public long getId() {return id;}
-	public void setId(long id) {this.id = id;}
-	public String getNome() {return nome;}
-	public void setNome(String nome) {this.nome = nome;}
-	public boolean isAtivo() {return ativo;}
-	public void setAtivo(boolean ativo) {this.ativo = ativo;}
-	public String getProdutor() {return produtor;}
-	public void setProdutor(String porduto) {this.produtor = produtor;}	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public boolean isAtivo() {
+		return ativo;
+	}
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
 	
 }
